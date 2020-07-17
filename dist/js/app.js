eb0cf9eedd4d98eef2f62070e670a859ddb6820f
Colin-nwk <collinsnwoko05@gmail.com>
2020-07-18 00:48:56 +0100

    functionality started

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,27 +1,8 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+// TODO : add items on new todo lists using js
+const todoForm = document.querySelector("#todo-form");
+const todo = document.querySelector("#input-todo");
 
-// select tab function item
-function selectItem(e) {
-	removeShowContent();
-	removeBorder();
-	// border to current tab
-
-	this.classList.add('tab-border');
-	// grab content item from DOM
-	console.log(this.id);
-	const tabContentItemShow = document.querySelector(`#${this.id}-content`);
-	//add shhow class
-	tabContentItemShow.classList.add('show');
-}
-
-//remove border
-function removeShowContent() {
-	tabContentItems.forEach((item) => item.classList.remove('show'));
-}
-function removeBorder() {
-	tabItems.forEach((item) => item.classList.remove('tab-border'));
-}
-
-// listener for tab click
-tabItems.forEach((item) => item.addEventListener('click', selectItem));
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(todo.value);
+});
